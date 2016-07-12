8515c665e5757bf0861a968f3e3e6ba49a0434c7
Andrew Swerlick <andrew.swerlick@hbdi.com>
2016-07-12 16:59:33 -0400

    removing old reference to quadrants.css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from 'react'
 import _ from 'lodash'
 
 import {ReactRenderer, ReactParser} from './marked-react'
-import quadrantStyles from 'quadrants.css'
 
 export class Markdown extends React.Component{
 
